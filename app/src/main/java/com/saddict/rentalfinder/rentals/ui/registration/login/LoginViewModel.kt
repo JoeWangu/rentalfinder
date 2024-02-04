2c6e704f1f79809615e6fe91f0816bdc926ab984
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-02-04 22:43:18 +0300

    added logic

--- app/src/main/java/com/saddict/rentalfinder/rentals/ui/registration/login/LoginViewModel.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/ui/registration/login/LoginViewModel.kt
@@ -1,4 +1,44 @@
 package com.saddict.rentalfinder.rentals.ui.registration.login
 
-class LoginViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.saddict.rentalfinder.rentals.data.manager.AppUiState
+import com.saddict.rentalfinder.rentals.data.manager.PreferenceDataStore
+import com.saddict.rentalfinder.rentals.data.remote.remository.RemoteDataSource
+import com.saddict.rentalfinder.rentals.data.usecase.LoginUseCase
+import com.saddict.rentalfinder.rentals.model.remote.LoginUser
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okio.IOException
+import javax.inject.Inject
+
+sealed interface LoginUiState {
+    data class Success(val userResponse: LoginUser) : LoginUiState
+    data object Error : LoginUiState
+    data object Loading : LoginUiState
+}
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val preferenceDataStore: PreferenceDataStore,
+    private val remoteDataSource: RemoteDataSource
+) : ViewModel() {
+    suspend fun loginUser(user: LoginUser) {
+        try {
+            withContext(Dispatchers.IO) {
+                val login = LoginUseCase(remoteDataSource).loginUser(user)
+                if (login.isSuccessful) {
+                    val token = login.body()?.token
+                    preferenceDataStore.setToken(token)
+                    AppUiState.Success
+                } else {
+                    AppUiState.Error
+                }
+            }
+        } catch (e: IOException) {
+            Log.d("Login Failure", "cannot login the user")
+            AppUiState.Error
+        }
+    }
 }
