c8960b7910f6430385121f1380e963ddf4078c0e
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-02-08 18:40:32 +0300

    add file

---
+++ app/src/main/java/com/saddict/rentalfinder/rentals/model/remote/register/RegisterUserResponseUser.kt
@@ -0,0 +1,24 @@
+package com.saddict.rentalfinder.rentals.model.remote.register
+
+import androidx.annotation.Keep
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@Keep
+data class RegisterUserResponseUser(
+    @JsonProperty("first_name")
+    val firstName: String,
+    @JsonProperty("last_name")
+    val lastName: String,
+    @JsonProperty("email")
+    val email: String,
+    @JsonProperty("username")
+    val username: String,
+    @JsonProperty("phone_number")
+    val phoneNumber: String,
+    @JsonProperty("address")
+    val address: String,
+    @JsonProperty("dob")
+    val dob: String?,
+    @JsonProperty("gender")
+    val gender: String?
+)
