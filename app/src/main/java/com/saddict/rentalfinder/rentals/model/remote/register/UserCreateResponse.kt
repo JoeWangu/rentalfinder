0f5bf199775d90a1c01215ead636e789e1ab7111
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-02-08 18:42:23 +0300

    comment out

---
+++ app/src/main/java/com/saddict/rentalfinder/rentals/model/remote/register/UserCreateResponse.kt
@@ -0,0 +1,12 @@
+package com.saddict.rentalfinder.rentals.model.remote.register
+
+//import androidx.annotation.Keep
+//import com.fasterxml.jackson.annotation.JsonProperty
+//
+//@Keep
+//data class UserCreateResponse(
+//    @JsonProperty("email")
+//    val email: String,
+//    @JsonProperty("username")
+//    val username: String
+//)
