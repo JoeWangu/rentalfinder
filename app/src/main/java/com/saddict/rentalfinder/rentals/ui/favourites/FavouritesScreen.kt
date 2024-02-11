d3d24ebd71627ca99a504699d98b1bcbbded2d14
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-02-11 11:25:37 +0300

    comment unused code

--- app/src/main/java/com/saddict/rentalfinder/rentals/ui/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/ui/favourites/FavouritesScreen.kt
@@ -1,42 +1,21 @@
 package com.saddict.rentalfinder.rentals.ui.favourites
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.saddict.rentalfinder.R
-import com.saddict.rentalfinder.rentals.model.remote.RentalResults
 import com.saddict.rentalfinder.rentals.ui.navigation.NavigationDestination
-import com.saddict.rentalfinder.utils.FavButton
 import com.saddict.rentalfinder.utils.utilscreens.RFABottomBar
 import com.saddict.rentalfinder.utils.utilscreens.RFATopBar
 
@@ -102,6 +81,7 @@ fun FavouritesBody(modifier: Modifier = Modifier) {
     )
 }
 
+/*
 @Composable
 fun FavouritesCard(
     rental: RentalResults,
@@ -166,4 +146,4 @@ fun FavouritesCard(
         }
 
     }
-}
+}*/
