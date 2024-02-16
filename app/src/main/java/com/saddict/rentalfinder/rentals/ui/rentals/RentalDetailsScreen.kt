363c5f10f7f99555eb44d4ce62e6ad2a05efc2f4
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-02-16 18:34:48 +0300

    added vertical scroll

--- app/src/main/java/com/saddict/rentalfinder/rentals/ui/rentals/RentalDetailsScreen.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/ui/rentals/RentalDetailsScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Divider
@@ -62,8 +64,9 @@ fun RentalDetailsScreen(
     uiState: State<RenDetailsUiState> = viewModel.uiState.collectAsState(),
     rental: RentalEntity = uiState.value.rentalDetails,
 ) {
+    val state = rememberScrollState()
     Column(
-        modifier = modifier
+        modifier = modifier.verticalScroll(state)
     ) {
         PropertyImage(
             rentalUrl = rental.imageUrl,
