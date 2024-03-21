f7cd0778c93ffdf014790887b0040ae4632f2bf0
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-03-21 11:40:55 +0300

    comment out unused code

--- app/src/main/java/com/saddict/rentalfinder/rentals/ui/images/RentalImageViewModel.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/ui/images/RentalImageViewModel.kt
@@ -1,48 +1,55 @@
 package com.saddict.rentalfinder.rentals.ui.images
 
-import android.util.Log
+//import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.saddict.rentalfinder.rentals.data.remote.remository.RemoteDataSource
-import com.saddict.rentalfinder.rentals.model.remote.RentalImage
+//import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingData
+//import com.saddict.rentalfinder.rentals.data.remote.remository.RemoteDataSource
+import com.saddict.rentalfinder.rentals.model.local.ImageEntity
+//import com.saddict.rentalfinder.rentals.model.remote.RentalImage
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+//import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+//import kotlinx.coroutines.flow.MutableSharedFlow
+//import kotlinx.coroutines.flow.SharedFlow
+//import kotlinx.coroutines.launch
+//import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-sealed interface ImagesUiState {
-    data class Success(val images: List<RentalImage>) : ImagesUiState
-    data object Error : ImagesUiState
-    data object Empty : ImagesUiState
-    data object Loading : ImagesUiState
-}
+//sealed interface ImagesUiState {
+//    data class Success(val images: List<RentalImage>) : ImagesUiState
+//    data object Error : ImagesUiState
+//    data object Empty : ImagesUiState
+//    data object Loading : ImagesUiState
+//}
 
 @HiltViewModel
 class RentalImageViewModel @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+//    private val remoteDataSource: RemoteDataSource,
+    pager: Pager<Int, ImageEntity>
 ) : ViewModel() {
-    private val _uiState = MutableSharedFlow<ImagesUiState>()
-    val uiState: SharedFlow<ImagesUiState> = _uiState
+//    private val _uiState = MutableSharedFlow<ImagesUiState>()
+//    val uiState: SharedFlow<ImagesUiState> = _uiState
 
-    fun getImages() {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                try {
-                    _uiState.emit(ImagesUiState.Loading)
-                    val images = remoteDataSource.getImages(1)
-                    if (images.imageList.isNotEmpty()) {
-                        _uiState.emit(ImagesUiState.Success(images.imageList))
-                    } else {
-                        _uiState.emit(ImagesUiState.Empty)
-                    }
-                } catch (e: Exception) {
-                    Log.d("Cannot load images", "getImages: $e")
-                    _uiState.emit(ImagesUiState.Error)
-                }
-            }
-        }
-    }
+//    fun getImages() {
+//        viewModelScope.launch {
+//            withContext(Dispatchers.IO) {
+//                try {
+//                    _uiState.emit(ImagesUiState.Loading)
+//                    val images = remoteDataSource.getImages(1)
+//                    if (images.imageList.isNotEmpty()) {
+//                        _uiState.emit(ImagesUiState.Success(images.imageList))
+//                    } else {
+//                        _uiState.emit(ImagesUiState.Empty)
+//                    }
+//                } catch (e: Exception) {
+//                    Log.d("Cannot load images", "getImages: $e")
+//                    _uiState.emit(ImagesUiState.Error)
+//                }
+//            }
+//        }
+//    }
+
+    val images: Flow<PagingData<ImageEntity>> = pager.flow
 }
