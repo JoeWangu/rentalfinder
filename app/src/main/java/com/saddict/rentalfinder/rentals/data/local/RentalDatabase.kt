a96662629a27e97e350c8f93a5aa6988a154a614
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-03-27 01:24:29 +0300

    add rentals dao

--- app/src/main/java/com/saddict/rentalfinder/rentals/data/local/RentalDatabase.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/data/local/RentalDatabase.kt
@@ -3,19 +3,22 @@ package com.saddict.rentalfinder.rentals.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.saddict.rentalfinder.rentals.model.local.ImageEntity
-import com.saddict.rentalfinder.rentals.model.local.RemoteKeys
+import com.saddict.rentalfinder.rentals.model.local.ImageRemoteKeys
 import com.saddict.rentalfinder.rentals.model.local.RentalEntity
+import com.saddict.rentalfinder.rentals.model.local.RentalRemoteKeys
 
 @Database(
     entities = [
         RentalEntity::class,
+        RentalRemoteKeys::class,
         ImageEntity::class,
-        RemoteKeys::class
+        ImageRemoteKeys::class
     ],
     version = 1,
     exportSchema = false
 )
 abstract class RentalDatabase : RoomDatabase() {
     abstract fun rentalDao(): RentalDao
-    abstract fun remoteKeysDao(): RemoteKeysDao
+    abstract fun imageRemoteKeysDao(): ImageRemoteKeysDao
+    abstract fun rentalRemoteKeysDao(): RentalRemoteKeysDao
 }
