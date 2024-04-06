22b3007402a049f8dd4b6204da8da1be5b030eba
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-04-06 23:50:22 +0300

    clear database on logout

--- app/src/main/java/com/saddict/rentalfinder/rentals/ui/profile/settings/SettingsViewModel.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/ui/profile/settings/SettingsViewModel.kt
@@ -1,15 +1,19 @@
 package com.saddict.rentalfinder.rentals.ui.profile.settings
 
 import androidx.lifecycle.ViewModel
+import com.saddict.rentalfinder.rentals.data.local.locasitory.LocalDataSource
 import com.saddict.rentalfinder.rentals.data.manager.PreferenceDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val preferenceDataStore: PreferenceDataStore
+    private val preferenceDataStore: PreferenceDataStore,
+    private val localDataSource: LocalDataSource
 ) : ViewModel() {
     suspend fun logout() {
         preferenceDataStore.setToken("")
+        localDataSource.deleteAllRentals()
+        localDataSource.deleteAllImages()
     }
 }
