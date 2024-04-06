a8c90dceeeaf81f1a6efd3ab59c9c129d0a5c6a2
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-04-06 23:51:03 +0300

    add rentalmanage pager rename parameters

--- app/src/main/java/com/saddict/rentalfinder/rentals/data/repository/Repository.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/data/repository/Repository.kt
@@ -5,42 +5,72 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.saddict.rentalfinder.rentals.data.local.RentalDatabase
+import com.saddict.rentalfinder.rentals.data.local.locasitory.LocalDataSource
 import com.saddict.rentalfinder.rentals.data.manager.ImageRemoteMediator
+import com.saddict.rentalfinder.rentals.data.manager.RentalManageRemoteMediator
 import com.saddict.rentalfinder.rentals.data.manager.RentalRemoteMediator
+import com.saddict.rentalfinder.rentals.data.remote.remository.RemoteDataSource
 import com.saddict.rentalfinder.rentals.model.local.ImageEntity
 import com.saddict.rentalfinder.rentals.model.local.RentalEntity
-import com.saddict.rentalfinder.rentals.network.RentalService
+import com.saddict.rentalfinder.rentals.model.local.RentalManageEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private val appApi: RentalService,
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
     private val appDatabase: RentalDatabase
 ) {
     @OptIn(ExperimentalPagingApi::class)
-    fun getAllImages(): Flow<PagingData<ImageEntity>> {
-        val pagingSourceFactory = { appDatabase.rentalDao().getAllPagedImages() }
+    fun getPagedRentals(): Flow<PagingData<RentalEntity>> {
+        val pagingSourceFactory = { localDataSource.fetchPagedRentals() }
         return Pager(
             config = PagingConfig(
                 pageSize = 10,
                 prefetchDistance = 1,
                 initialLoadSize = 20
             ),
-            remoteMediator = ImageRemoteMediator(appApi, appDatabase),
+            remoteMediator = RentalRemoteMediator(
+                remoteDataSource = remoteDataSource,
+                localDataSource = localDataSource,
+                appDatabase = appDatabase
+            ),
             pagingSourceFactory = pagingSourceFactory
         ).flow
     }
 
     @OptIn(ExperimentalPagingApi::class)
-    fun getAllRentals(): Flow<PagingData<RentalEntity>> {
-        val pagingSourceFactory = { appDatabase.rentalDao().fetchAllPagedRentals() }
+    fun getPagedManageRentals(): Flow<PagingData<RentalManageEntity>> {
+        val pagingSourceFactory = { localDataSource.fetchPagedManageRentals() }
         return Pager(
             config = PagingConfig(
                 pageSize = 10,
                 prefetchDistance = 1,
                 initialLoadSize = 20
             ),
-            remoteMediator = RentalRemoteMediator(appApi, appDatabase),
+            remoteMediator = RentalManageRemoteMediator(
+                remoteDataSource = remoteDataSource,
+                localDataSource = localDataSource,
+                appDatabase = appDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+    @OptIn(ExperimentalPagingApi::class)
+    fun getPagedImages(): Flow<PagingData<ImageEntity>> {
+        val pagingSourceFactory = { localDataSource.fetchPagedImages() }
+        return Pager(
+            config = PagingConfig(
+                pageSize = 10,
+                prefetchDistance = 1,
+                initialLoadSize = 20
+            ),
+            remoteMediator = ImageRemoteMediator(
+                remoteDataSource = remoteDataSource,
+                localDataSource = localDataSource,
+                appDatabase = appDatabase
+            ),
             pagingSourceFactory = pagingSourceFactory
         ).flow
     }
