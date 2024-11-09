c86cc15713de59eeb36b8f8c6fc6d1d297d1dd5b
JoeWangu <joewangu2021@outlook.com>
2024-11-09 13:05:46 +0300

    add missing fields

--- app/src/main/java/com/saddict/rentalfinder/rentals/model/remote/Locations.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/model/remote/Locations.kt
@@ -5,14 +5,40 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @Keep
 data class Country(
+    @JsonProperty("count")
+    val count: Int,
+    @JsonProperty("next")
+    val next: String?,
+    @JsonProperty("previous")
+    val previous: String?,
+    @JsonProperty("results")
+    val results: List<CountryResults>
+)
+
+@Keep
+data class CountryResults(
     @JsonProperty("id")
     val id: Int,
     @JsonProperty("name")
-    val name: String
+    val name: String,
+    @JsonProperty("code")
+    val code: String
 )
 
 @Keep
 data class State(
+    @JsonProperty("count")
+    val count: Int,
+    @JsonProperty("next")
+    val next: String?,
+    @JsonProperty("previous")
+    val previous: String?,
+    @JsonProperty("results")
+    val results: List<StateResults>
+)
+
+@Keep
+data class StateResults(
     @JsonProperty("id")
     val id: Int,
     @JsonProperty("name")
@@ -23,16 +49,42 @@ data class State(
 
 @Keep
 data class City(
+    @JsonProperty("count")
+    val count: Int,
+    @JsonProperty("next")
+    val next: String?,
+    @JsonProperty("previous")
+    val previous: String?,
+    @JsonProperty("results")
+    val results: List<CityResults>
+)
+
+@Keep
+data class CityResults(
     @JsonProperty("id")
     val id: Int,
     @JsonProperty("name")
     val name: String,
     @JsonProperty("state")
-    val state: Int
+    val state: Int?,
+    @JsonProperty("country")
+    val country: Int
 )
 
 @Keep
 data class Neighborhood(
+    @JsonProperty("count")
+    val count: Int,
+    @JsonProperty("next")
+    val next: String?,
+    @JsonProperty("previous")
+    val previous: String?,
+    @JsonProperty("results")
+    val results: List<NeighborhoodResults>
+)
+
+@Keep
+data class NeighborhoodResults(
     @JsonProperty("id")
     val id: Int,
     @JsonProperty("name")
