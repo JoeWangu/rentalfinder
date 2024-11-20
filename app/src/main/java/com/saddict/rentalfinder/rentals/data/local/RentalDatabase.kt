7f8264eaef64c921a564b626fa4da265a4d864a5
JoeWangu <joewangu2021@outlook.com>
2024-11-21 00:54:36 +0300

    add user and profile entities

--- app/src/main/java/com/saddict/rentalfinder/rentals/data/local/RentalDatabase.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/data/local/RentalDatabase.kt
@@ -9,23 +9,26 @@ import com.saddict.rentalfinder.rentals.data.local.dao.ManageRentalDao
 import com.saddict.rentalfinder.rentals.data.local.dao.RentalDao
 import com.saddict.rentalfinder.rentals.data.local.dao.RentalManageRemoteKeysDao
 import com.saddict.rentalfinder.rentals.data.local.dao.RentalRemoteKeysDao
+import com.saddict.rentalfinder.rentals.data.local.dao.RentalUserDao
 import com.saddict.rentalfinder.rentals.data.local.dao.UserDao
 import com.saddict.rentalfinder.rentals.data.local.dao.UserProfileDao
+import com.saddict.rentalfinder.rentals.model.local.UserEntity
+import com.saddict.rentalfinder.rentals.model.local.UserProfileEntity
 import com.saddict.rentalfinder.rentals.model.local.images.ImageEntity
 import com.saddict.rentalfinder.rentals.model.local.images.ImageRemoteKeysEntity
 import com.saddict.rentalfinder.rentals.model.local.rentals.RentalEntity
 import com.saddict.rentalfinder.rentals.model.local.rentals.RentalManageEntity
 import com.saddict.rentalfinder.rentals.model.local.rentals.RentalManageRemoteKeysEntity
 import com.saddict.rentalfinder.rentals.model.local.rentals.RentalRemoteKeysEntity
-import com.saddict.rentalfinder.rentals.model.remote.User
-import com.saddict.rentalfinder.rentals.model.remote.UserProfile
-import com.saddict.rentalfinder.utils.UserProfileDetailsConverter
-import com.saddict.rentalfinder.utils.UserProfileTypeConverter
+import com.saddict.rentalfinder.rentals.model.remote.RentalUser
+import com.saddict.rentalfinder.utils.RentalUserProfileDetailsConverter
+import com.saddict.rentalfinder.utils.RentalUserProfileTypeConverter
 
 @Database(
     entities = [
-        UserProfile::class,
-        User::class,
+        UserEntity::class,
+        UserProfileEntity::class,
+        RentalUser::class,
         RentalEntity::class,
         RentalManageEntity::class,
         ImageEntity::class,
@@ -36,10 +39,11 @@ import com.saddict.rentalfinder.utils.UserProfileTypeConverter
     version = 1,
     exportSchema = false
 )
-@TypeConverters(UserProfileTypeConverter::class, UserProfileDetailsConverter::class)
+@TypeConverters(RentalUserProfileTypeConverter::class, RentalUserProfileDetailsConverter::class)
 abstract class RentalDatabase : RoomDatabase() {
-    abstract fun userProfileDao(): UserProfileDao
     abstract fun userDao(): UserDao
+    abstract fun userProfileDao(): UserProfileDao
+    abstract fun rentalUserDao(): RentalUserDao
     abstract fun rentalDao(): RentalDao
     abstract fun imageDao(): ImageDao
     abstract fun imageRemoteKeysDao(): ImageRemoteKeysDao
