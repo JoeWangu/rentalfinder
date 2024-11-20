020cb862fd83ff558cae1eb93ed2df92bf2f698a
JoeWangu <joewangu2021@outlook.com>
2024-11-21 00:59:11 +0300

    rename type converters

--- app/src/main/java/com/saddict/rentalfinder/utils/UserProfileTypeConverter.kt
+++ app/src/main/java/com/saddict/rentalfinder/utils/UserProfileTypeConverter.kt
@@ -3,33 +3,33 @@ package com.saddict.rentalfinder.utils
 import androidx.room.TypeConverter
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.saddict.rentalfinder.rentals.model.remote.UserProfile
-import com.saddict.rentalfinder.rentals.model.remote.UserProfileDetails
+import com.saddict.rentalfinder.rentals.model.local.UserProfileEntity
+import com.saddict.rentalfinder.rentals.model.remote.RentalUserProfileDetails
 
-class UserProfileTypeConverter {
+class RentalUserProfileTypeConverter {
     private val objectMapper = jacksonObjectMapper()
 
     @TypeConverter
-    fun fromUserProfile(userProfile: UserProfile?): String {
-        return userProfile.let { objectMapper.writeValueAsString(it) }
+    fun fromUserProfile(userProfileEntity: UserProfileEntity?): String {
+        return userProfileEntity.let { objectMapper.writeValueAsString(it) }
     }
 
     @TypeConverter
-    fun toUserProfile(userProfileString: String?): UserProfile? {
-        return userProfileString?.let { objectMapper.readValue<UserProfile>(it) }
+    fun toRentalUserProfile(rentalUserProfileString: String?): UserProfileEntity? {
+        return rentalUserProfileString?.let { objectMapper.readValue<UserProfileEntity>(it) }
     }
 }
 
-class UserProfileDetailsConverter {
+class RentalUserProfileDetailsConverter {
     private val objectMapper = jacksonObjectMapper()
 
     @TypeConverter
-    fun toUserProfileDetails(userProfileDetailsString: String?): UserProfileDetails? {
-        return userProfileDetailsString?.let { objectMapper.readValue<UserProfileDetails>(it) }
+    fun toUserProfileDetails(rentalUserProfileString: String?): RentalUserProfileDetails? {
+        return rentalUserProfileString?.let { objectMapper.readValue<RentalUserProfileDetails>(it) }
     }
 
     @TypeConverter
-    fun fromUserProfileDetails(userProfileDetails: UserProfileDetails?): String {
+    fun fromUserProfileDetails(userProfileDetails: RentalUserProfileDetails?): String {
         return userProfileDetails.let { objectMapper.writeValueAsString(it) }
     }
 }
