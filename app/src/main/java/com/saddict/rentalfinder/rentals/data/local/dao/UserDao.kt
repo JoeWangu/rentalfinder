77b01f428bc4119aafc991a6b147b505c3d9cf21
JoeWangu <joewangu2021@outlook.com>
2024-11-21 00:57:30 +0300

    rename User to UserEntity

--- app/src/main/java/com/saddict/rentalfinder/rentals/data/local/dao/UserDao.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/data/local/dao/UserDao.kt
@@ -4,17 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.saddict.rentalfinder.rentals.model.remote.User
+import com.saddict.rentalfinder.rentals.model.local.UserEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertUser(user: User)
+    suspend fun insertUser(user: UserEntity)
 
-    @Query("SELECT * FROM user")
-    fun fetchUser(): Flow<User>
+    @Query("SELECT * FROM user ORDER BY id DESC LIMIT 1")
+    fun getUser(): Flow<UserEntity>
 
     @Query("DELETE FROM user")
     suspend fun deleteUser()
+
 }
