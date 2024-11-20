dfb30f63add9ea8b8347e9923bf4bce8a6d3fac3
JoeWangu <joewangu2021@outlook.com>
2024-11-21 00:58:44 +0300

    Rename UserProfile to UserProfileEntity

--- app/src/main/java/com/saddict/rentalfinder/rentals/data/local/dao/UserProfileDao.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/data/local/dao/UserProfileDao.kt
@@ -4,16 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.saddict.rentalfinder.rentals.model.remote.UserProfile
+import com.saddict.rentalfinder.rentals.model.local.UserProfileEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserProfileDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertUserProfile(profile: UserProfile)
+    suspend fun insertUserProfile(profile: UserProfileEntity)
 
     @Query("SELECT * FROM user_profile")
-    fun fetchUserProfile(): Flow<UserProfile>
+    fun fetchUserProfile(): Flow<UserProfileEntity>
 
     @Query("DELETE FROM user_profile")
     suspend fun deleteUserProfile()
