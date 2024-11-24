42269122c640697fcd2b5ffef68c0f0d55c21a01
JoeWangu <joewangu2021@outlook.com>
2024-11-25 00:35:54 +0300

    remove console logs

--- app/src/main/java/com/saddict/rentalfinder/rentals/ui/registration/login/LoginViewModel.kt
+++ app/src/main/java/com/saddict/rentalfinder/rentals/ui/registration/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.saddict.rentalfinder.rentals.ui.registration.login
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.saddict.rentalfinder.rentals.data.local.locasitory.LocalDataSource
@@ -10,12 +9,14 @@ import com.saddict.rentalfinder.rentals.data.usecase.LoginUseCase
 import com.saddict.rentalfinder.rentals.model.local.UserEntity
 import com.saddict.rentalfinder.rentals.model.remote.register.LoginUser
 import com.saddict.rentalfinder.rentals.model.remote.register.LoginUserResponse
+import com.saddict.rentalfinder.utils.mapToUserProfileEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import javax.inject.Inject
 
 sealed interface LoginUiState {
@@ -59,15 +60,26 @@ class LoginViewModel @Inject constructor(
                         )
                         localDataSource.deleteUser()
                         localDataSource.insertUser(userEntity)
-                        Log.d("Success", response.toString())
+//                        Log.d("Success", response.toString())
                         _uiState.value = LoginUiState.Success(response!!)
+                        launch {
+                            try {
+                                val userProfile = remoteDataSource.getUserProfile()
+                                    ?.mapToUserProfileEntity()
+                                if (userProfile != null) {
+                                    localDataSource.insertUserProfile(userProfile)
+                                }
+                            } catch (_: HttpException) {
+                            } catch (_: Exception) {
+                            }
+                        }
                     } else {
                         _uiState.value = LoginUiState.Error
-                        val errorBody = login.raw()
-                        Log.e("NoSuccess", "Error: $errorBody")
+//                        val errorBody = login.raw()
+//                        Log.e("NoSuccess", "Error: $errorBody")
                     }
-                } catch (e: Exception) {
-                    Log.d("Login Failure", "cannot login the user: $e")
+                } catch (_: Exception) {
+//                    Log.d("Login Failure", "cannot login the user: $e")
                     _uiState.value = LoginUiState.NetError
                 }
             }
